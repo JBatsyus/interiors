f7ec150648cd6f4ecd089e3bbdbe0798c0e3e67f
JBatsyus <ju.kalabina@gmail.com>
2025-06-14 18:14:44 +0200

    примерная форма

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,24 @@ var swiperWorkflowGallery = new Swiper(".workflow-gallery__swiper", {
     },
 
 });
+
+document.querySelectorAll('.contact-form__tab').forEach(tab => {
+    tab.addEventListener('click', () => {
+        // Удаляем активный класс у всех табов
+        document.querySelectorAll('.contact-form__tab').forEach(t => {
+            t.classList.remove('active');
+        });
+
+        // Добавляем активный класс текущему табу
+        tab.classList.add('active');
+
+        // Скрываем все содержимое табов
+        document.querySelectorAll('.contact-form__pane').forEach(pane => {
+            pane.classList.remove('active');
+        });
+
+        // Показываем нужное содержимое
+        const tabName = tab.dataset.tab;
+        document.querySelector(`.contact-form__pane[data-tab-content="${tabName}"]`).classList.add('active');
+    });
+});
