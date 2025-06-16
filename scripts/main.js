9b6a64ff6579ad379e24fa333a31295c3adf0d8d
JBatsyus <ju.kalabina@gmail.com>
2025-06-16 17:24:54 +0200

    секция с формой + маска деск

--- scripts/main.js
+++ scripts/main.js
@@ -14,26 +14,26 @@ var swiperWorkflowGallery = new Swiper(".workflow-gallery__swiper", {
 
 });
 
-// document.querySelectorAll('.contact-form__tab').forEach(tab => {
-//     tab.addEventListener('click', () => {
-//         // Удаляем активный класс у всех табов
-//         document.querySelectorAll('.contact-form__tab').forEach(t => {
-//             t.classList.remove('active');
-//         });
+document.querySelectorAll('.contact-form__tab').forEach(tab => {
+    tab.addEventListener('click', () => {
+        // Удаляем активный класс у всех табов
+        document.querySelectorAll('.contact-form__tab').forEach(t => {
+            t.classList.remove('active');
+        });
 
-//         // Добавляем активный класс текущему табу
-//         tab.classList.add('active');
+        // Добавляем активный класс текущему табу
+        tab.classList.add('active');
 
-//         // Скрываем все содержимое табов
-//         document.querySelectorAll('.contact-form__pane').forEach(pane => {
-//             pane.classList.remove('active');
-//         });
+        // Скрываем все содержимое табов
+        document.querySelectorAll('.contact-form__pane').forEach(pane => {
+            pane.classList.remove('active');
+        });
 
-//         // Показываем нужное содержимое
-//         const tabName = tab.dataset.tab;
-//         document.querySelector(`.contact-form__pane[data-tab-content="${tabName}"]`).classList.add('active');
-//     });
-// });
+        // Показываем нужное содержимое
+        const tabName = tab.dataset.tab;
+        document.querySelector(`.contact-form__pane[data-tab-content="${tabName}"]`).classList.add('active');
+    });
+});
 
 
 const menuHumb = document.querySelector('.menu-humb');
@@ -65,3 +65,8 @@ document.querySelectorAll('.menu-mob a').forEach(link => {
         document.body.classList.remove('no-scroll');
     });
 });
+
+/// mask
+	$(function () {
+		$(".input-phone").mask("+7 (999) 999 - 99 - 99");
+	});
