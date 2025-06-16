49b83460102802b97ed54f78cc8201ae15a0ec5a
JBatsyus <ju.kalabina@gmail.com>
2025-06-16 11:29:07 +0200

    мобильное меню + правки

--- scripts/main.js
+++ scripts/main.js
@@ -14,23 +14,54 @@ var swiperWorkflowGallery = new Swiper(".workflow-gallery__swiper", {
 
 });
 
-document.querySelectorAll('.contact-form__tab').forEach(tab => {
-    tab.addEventListener('click', () => {
-        // Удаляем активный класс у всех табов
-        document.querySelectorAll('.contact-form__tab').forEach(t => {
-            t.classList.remove('active');
-        });
+// document.querySelectorAll('.contact-form__tab').forEach(tab => {
+//     tab.addEventListener('click', () => {
+//         // Удаляем активный класс у всех табов
+//         document.querySelectorAll('.contact-form__tab').forEach(t => {
+//             t.classList.remove('active');
+//         });
 
-        // Добавляем активный класс текущему табу
-        tab.classList.add('active');
+//         // Добавляем активный класс текущему табу
+//         tab.classList.add('active');
 
-        // Скрываем все содержимое табов
-        document.querySelectorAll('.contact-form__pane').forEach(pane => {
-            pane.classList.remove('active');
-        });
+//         // Скрываем все содержимое табов
+//         document.querySelectorAll('.contact-form__pane').forEach(pane => {
+//             pane.classList.remove('active');
+//         });
 
-        // Показываем нужное содержимое
-        const tabName = tab.dataset.tab;
-        document.querySelector(`.contact-form__pane[data-tab-content="${tabName}"]`).classList.add('active');
+//         // Показываем нужное содержимое
+//         const tabName = tab.dataset.tab;
+//         document.querySelector(`.contact-form__pane[data-tab-content="${tabName}"]`).classList.add('active');
+//     });
+// });
+
+
+const menuHumb = document.querySelector('.menu-humb');
+const menuMob = document.querySelector('.menu-mob');
+const header = document.querySelector('.header');
+
+// Обработчик клика на бургер
+menuHumb.addEventListener('click', function (e) {
+    e.stopPropagation(); // Предотвращаем всплытие
+    this.classList.toggle('active');
+    menuMob.classList.toggle('active');
+    document.body.classList.toggle('no-scroll');
+});
+
+// Закрытие меню при клике вне его
+document.addEventListener('click', function (e) {
+    if (!e.target.closest('.menu-mob') && !e.target.closest('.menu-humb')) {
+        menuHumb.classList.remove('active');
+        menuMob.classList.remove('active');
+        document.body.classList.remove('no-scroll');
+    }
+});
+
+// Закрытие при клике на ссылки
+document.querySelectorAll('.menu-mob a').forEach(link => {
+    link.addEventListener('click', () => {
+        menuHumb.classList.remove('active');
+        menuMob.classList.remove('active');
+        document.body.classList.remove('no-scroll');
     });
 });
